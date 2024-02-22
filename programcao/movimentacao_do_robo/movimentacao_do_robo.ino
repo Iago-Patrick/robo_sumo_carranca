a1a83a149abc06ebfef98acf51f5761548befbe6
Iago-patrick <iagopatrick14@gmail.com>
2024-02-22 00:31:12 -0300

    codigo finalizado

--- programcao/movimentacao_do_robo/movimentacao_do_robo.ino
+++ programcao/movimentacao_do_robo/movimentacao_do_robo.ino
@@ -7,12 +7,60 @@
 #define ch7   8 //Canal 7 do rádio instanciado à entrada digital 8
 #define ch8   9 //Canal 8 do rádio instanciado à entrada digital 9
 
-#define arma 10
-#define motor_1 11
-#define motor_2 12
-
 #define LED  13 //LED onboard 
 
+// PORTAS DA PONTE H
+#define dirFrente 5
+#define dirTras   3
+#define esqFrente 9
+#define esqTras   6
+#define armFrente 11
+#define armTras   10
+
+#define velocidadeE 110
+#define velocidadeD 110
+
+void frente()
+{
+    // MOTOR DIREITO PARA FRENTE
+    analogWrite(dirFrente,  velocidadeD);
+    analogWrite(dirTras,    LOW);
+
+    // MOTOR ESQUERDO PARA FRENTE
+    analogWrite(esqFrente,  velocidadeE);
+    analogWrite(esqTras,    LOW);
+}
+void tras ()
+{
+    // MOTOR DIREITO PARA TRAS
+    analogWrite(dirFrente,  LOW);
+    analogWrite(dirTras,    velocidadeE);
+
+    // MOTOR ESQUERDO PARA TRAS
+    analogWrite(esqFrente,  LOW);
+    analogWrite(esqTras,    velocidadeD);
+}
+
+void direita()
+{
+    // MOTOR DIREITO PARA TRAS
+    analogWrite(dirFrente,  LOW);
+    analogWrite(dirTras,    velocidadeD);
+
+    // MOTOR ESQUERDO PARA FRENTE
+    analogWrite(esqFrente,  velocidadeE);
+    analogWrite(esqTras,    LOW);
+}
+void esquerda()
+{
+    // MOTOR DIREITO PARA FRENTE
+    analogWrite(dirFrente,  velocidadeD);
+    analogWrite(dirTras,    LOW);
+
+    // MOTOR ESQUERDO PARA TRAS
+    analogWrite(esqFrente,  LOW);
+    analogWrite(esqTras,    velocidadeE);
+}
  
 // --- Protótipo das funções auxiliares ---
 void read_channels();      //Função para leitura das entradas dos canais
@@ -47,45 +95,6 @@ ISR(TIMER2_OVF_vect)
     
 } //end ISR
 
-if(canal_01 > 1500) 
-{
-  digitalWrite(motor1, HIGH);
-   digitalWrite(motor2, HIGH);
-   else if(canal_01 < 1100) 
-   {
-   digitalWrite(motor1, LOW);
-   digitalWrite(motor2, LOW);
-   
-
-   }
-}
-if(canal_02 > 1500) 
-{
-  digitalWrite(motor1, HIGH);
-   digitalWrite(motor2, LOW);
-   else if(canal_02 < 1100)
-   { 
-   digitalWrite(motor1, LOW);
-   digitalWrite(motor2, HIGH);
-
-}
-}
-if(canal_03 > 1550)
-{
-digitalWrite(arma, HIGH);
-
-else if(canal03 < 1100)
-{
-digitalWrite(arma, LOW);    
-
-}
-
-
-}
-
- 
-
-
 // --- Configurações iniciais ---
 void setup()
 { 
@@ -107,17 +116,26 @@ void setup()
      
   Serial.begin(9600);            //Inicia comunicação Serial com 9600 de baud rate
   
-  
   // -- Registradores de configuração do Timer2 --
      TCCR2A = 0x00;   //Timer operando em modo normal
      TCCR2B = 0x07;   //Prescaler 1:1024
      TCNT2  = 100;    //10 ms overflow again
      TIMSK2 = 0x01;   //Habilita interrupção do Timer2
   
-   
-  
-} //end setup
 
+  // CONFIGURAÇÃO DOS PINOS DA PONTE-H
+  pinMode(dirFrente, OUTPUT);
+  pinMode(dirTras,   OUTPUT);
+  pinMode(esqFrente, OUTPUT);
+  pinMode(esqTras,   OUTPUT);
+
+  // DEIXANDO OS MOTORES PARADOS
+  digitalWrite(dirFrente, LOW);
+  digitalWrite(dirTras,   LOW);
+  digitalWrite(esqFrente, LOW);
+  digitalWrite(esqTras,   LOW);
+   
+} //end setup
 
 //Loop infinito
 void loop()
@@ -126,12 +144,34 @@ void loop()
    
     test_channels(); //Testa os canais e envia informação para o Serial Monitor
     
-    
-    delay(1000);
-    
-    
-} //end loop
+  if(canal_01 > 1500) //direita 
+    direita();
+  else 
+    if(canal_01 < 1100) //esquerda
+      esquerda();
+  if(canal_02 > 1500) //cima
+    frente();
+  else 
+    if(canal_02 < 1100) //baixo
+      tras();
+  if(canal_03 > 1550)
+  {
+    analogWrite(armFrente,  velocidadeD);
+    analogWrite(armTras,    LOW);
 
+    analogWrite(armFrente,  velocidadeE);
+    analogWrite(armTras,    LOW);
+  }
+  else 
+    if(canal_03 < 1100)
+    {
+      analogWrite(armTras,  velocidadeD);
+      analogWrite(armFrente,    LOW);
+
+      analogWrite(armTras,  velocidadeE);
+      analogWrite(armFrente,    LOW);
+    }  
+}//end loop
 
 //Funções auxiliares
 void read_channels() //Faz a leitura dos 6 primeiros canais do rádio
@@ -144,15 +184,10 @@ void read_channels() //Faz a leitura dos 6 primeiros canais do rádio
   canal_06 = pulseIn(ch6, HIGH, 25000); //Lê pulso em nível alto do canal 6 e armazena na variável canal_06
   canal_07 = pulseIn(ch7, HIGH, 25000); //Lê pulso em nível alto do canal 7 e armazena na variável canal_07
   canal_08 = pulseIn(ch8, HIGH, 25000); //Lê pulso em nível alto do canal 8 e armazena na variável canal_08
-   
-
 } //end read channels
-pulseIn (ch1, HIGH, 25000);
 
 void test_channels() //Testa os canais via serial monitor (comentar esta função e só chamar quando necessário)
-{
-  
-   
+{  
       Serial.print("Valor:  ");
       Serial.print(canal_01);
       Serial.print(" | ");
