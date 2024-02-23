2a904c829eddf43c3643fd4be69b92745a3a2e1b
lss1808 <lss10@poli.br>
2024-02-23 10:58:11 -0300

    declaracao das entradas dos pinos no codigo

--- programcao/movimentacao_do_robo/movimentacao_do_robo.ino
+++ programcao/movimentacao_do_robo/movimentacao_do_robo.ino
@@ -1,19 +1,19 @@
-#define ch1   2 //Canal 1 do rádio instanciado à entrada digital 2
-#define ch2   3 //Canal 2 do rádio instanciado à entrada digital 3
-#define ch3   4 //Canal 3 do rádio instanciado à entrada digital 4
-#define ch4   5 //Canal 4 do rádio instanciado à entrada digital 5
+#define ch1   6 //Canal 1 do rádio instanciado à entrada digital 2
+#define ch2   5 //Canal 2 do rádio instanciado à entrada digital 3
+#define ch3   3 //Canal 3 do rádio instanciado à entrada digital 4
+/*#define ch4   5 //Canal 4 do rádio instanciado à entrada digital 5
 #define ch5   6 //Canal 5 do rádio instanciado à entrada digital 6
 #define ch6   7 //Canal 6 do rádio instanciado à entrada digital 7
 #define ch7   8 //Canal 7 do rádio instanciado à entrada digital 8
 #define ch8   9 //Canal 8 do rádio instanciado à entrada digital 9
-
+*/
 #define LED  13 //LED onboard 
-
+//entradas dos motores 11, 10 , 9, 8
 // PORTAS DA PONTE H
-#define dirFrente 5
-#define dirTras   3
+#define dirFrente 10
+#define dirTras  11
 #define esqFrente 9
-#define esqTras   6
+#define esqTras   8
 #define armFrente 11
 #define armTras   10
 
@@ -73,13 +73,13 @@ void test_channels();      //Testa os 8 canais do Turnigy9x
 int canal_01 = 0, 
     canal_02 = 0, 
     canal_03 = 0, 
-    canal_04 = 0, 
+   /* canal_04 = 0, 
     canal_05 = 0, 
     canal_06 = 0, 
     canal_07 = 0, 
     canal_08 = 0; 
     
- 
+ */
     
     
 // --- Rotina de Interrupção ---
@@ -103,12 +103,12 @@ void setup()
   pinMode(ch1, INPUT); //Entrada para o canal 1 do rádio
   pinMode(ch2, INPUT); //Entrada para o canal 2 do rádio
   pinMode(ch3, INPUT); //Entrada para o canal 3 do rádio
-  pinMode(ch4, INPUT); //Entrada para o canal 4 do rádio
+/*  pinMode(ch4, INPUT); //Entrada para o canal 4 do rádio
   pinMode(ch5, INPUT); //Entrada para o canal 5 do rádio
   pinMode(ch6, INPUT); //Entrada para o canal 6 do rádio
   pinMode(ch7, INPUT); //Entrada para o canal 7 do rádio
   pinMode(ch8, INPUT); //Entrada para o canal 8 do rádio
-  
+  */
   pinMode(LED, OUTPUT); //saída para o LED onboard
   
   digitalWrite(LED, LOW); //LED inicia desligado
@@ -179,11 +179,12 @@ void read_channels() //Faz a leitura dos 6 primeiros canais do rádio
   canal_01 = pulseIn(ch1, HIGH, 25000); //Lê pulso em nível alto do canal 1 e armazena na variável canal_01
   canal_02 = pulseIn(ch2, HIGH, 25000); //Lê pulso em nível alto do canal 2 e armazena na variável canal_02
   canal_03 = pulseIn(ch3, HIGH, 25000); //Lê pulso em nível alto do canal 3 e armazena na variável canal_03
-  canal_04 = pulseIn(ch4, HIGH, 25000); //Lê pulso em nível alto do canal 4 e armazena na variável canal_04
+  /*canal_04 = pulseIn(ch4, HIGH, 25000); //Lê pulso em nível alto do canal 4 e armazena na variável canal_04
   canal_05 = pulseIn(ch5, HIGH, 25000); //Lê pulso em nível alto do canal 5 e armazena na variável canal_05
   canal_06 = pulseIn(ch6, HIGH, 25000); //Lê pulso em nível alto do canal 6 e armazena na variável canal_06
   canal_07 = pulseIn(ch7, HIGH, 25000); //Lê pulso em nível alto do canal 7 e armazena na variável canal_07
   canal_08 = pulseIn(ch8, HIGH, 25000); //Lê pulso em nível alto do canal 8 e armazena na variável canal_08
+*/
 } //end read channels
 
 void test_channels() //Testa os canais via serial monitor (comentar esta função e só chamar quando necessário)
@@ -195,7 +196,7 @@ void test_channels() //Testa os canais via serial monitor (comentar esta funçã
       Serial.print(" | ");
       Serial.print(canal_03);
       Serial.print(" | ");
-      Serial.print(canal_04);
+/*      Serial.print(canal_04);
       Serial.print(" | ");
       Serial.print(canal_05);
       Serial.print(" | ");
@@ -205,6 +206,7 @@ void test_channels() //Testa os canais via serial monitor (comentar esta funçã
       Serial.print(" | ");
       Serial.print(canal_08);
       Serial.print(" | ");
+      */
       Serial.println("");
       Serial.println("");
       Serial.print("Canal:  ");
@@ -214,7 +216,7 @@ void test_channels() //Testa os canais via serial monitor (comentar esta funçã
       Serial.print("|");
       Serial.print("  03  ");
       Serial.print("|");
-      Serial.print("  04  ");
+     /* Serial.print("  04  ");
       Serial.print("|");
       Serial.print("  05  ");
       Serial.print("|");
@@ -223,7 +225,7 @@ void test_channels() //Testa os canais via serial monitor (comentar esta funçã
       Serial.print("  07  ");
       Serial.print("|");
       Serial.print("  08  ");
-      Serial.print("|");
+      Serial.print("|");*/
       Serial.println("");
       Serial.println("");
 }
