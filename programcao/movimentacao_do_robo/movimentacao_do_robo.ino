0e9f0910974508428619a846422ed1ff0c37cdd3
Iago-patrick <iagopatrick14@gmail.com>
2024-02-25 19:06:45 -0300

    adicionado a função map

--- programcao/movimentacao_do_robo/movimentacao_do_robo.ino
+++ programcao/movimentacao_do_robo/movimentacao_do_robo.ino
@@ -1,12 +1,7 @@
 #define ch1   6 //Canal 1 do rádio instanciado à entrada digital 2
 #define ch2   5 //Canal 2 do rádio instanciado à entrada digital 3
 #define ch3   3 //Canal 3 do rádio instanciado à entrada digital 4
-/*#define ch4   5 //Canal 4 do rádio instanciado à entrada digital 5
-#define ch5   6 //Canal 5 do rádio instanciado à entrada digital 6
-#define ch6   7 //Canal 6 do rádio instanciado à entrada digital 7
-#define ch7   8 //Canal 7 do rádio instanciado à entrada digital 8
-#define ch8   9 //Canal 8 do rádio instanciado à entrada digital 9
-*/
+
 #define LED  13 //LED onboard 
 //entradas dos motores 11, 10 , 9, 8
 // PORTAS DA PONTE H
@@ -16,50 +11,52 @@
 #define esqTras   8
 #define armFrente 11
 #define armTras   10
+#define velocidadeG 110
 
-#define velocidadeE 110
-#define velocidadeD 110
+int velocidade =0;
 
-void frente()
+
+
+void frente(int velocidade)
 {
     // MOTOR DIREITO PARA FRENTE
-    analogWrite(dirFrente,  velocidadeD);
+    analogWrite(dirFrente,  velocidade);
     analogWrite(dirTras,    LOW);
 
     // MOTOR ESQUERDO PARA FRENTE
-    analogWrite(esqFrente,  velocidadeE);
+    analogWrite(esqFrente,  velocidade);
     analogWrite(esqTras,    LOW);
 }
-void tras ()
+void tras (int velocidade)
 {
     // MOTOR DIREITO PARA TRAS
     analogWrite(dirFrente,  LOW);
-    analogWrite(dirTras,    velocidadeE);
+    analogWrite(dirTras,    velocidade);
 
     // MOTOR ESQUERDO PARA TRAS
     analogWrite(esqFrente,  LOW);
-    analogWrite(esqTras,    velocidadeD);
+    analogWrite(esqTras,    velocidade);
 }
 
-void direita()
+void direita(int velocidade)
 {
     // MOTOR DIREITO PARA TRAS
     analogWrite(dirFrente,  LOW);
-    analogWrite(dirTras,    velocidadeD);
+    analogWrite(dirTras,    velocidade);
 
     // MOTOR ESQUERDO PARA FRENTE
-    analogWrite(esqFrente,  velocidadeE);
+    analogWrite(esqFrente,  velocidade);
     analogWrite(esqTras,    LOW);
 }
-void esquerda()
+void esquerda(int velocidade)
 {
     // MOTOR DIREITO PARA FRENTE
-    analogWrite(dirFrente,  velocidadeD);
+    analogWrite(dirFrente,  velocidade);
     analogWrite(dirTras,    LOW);
 
     // MOTOR ESQUERDO PARA TRAS
     analogWrite(esqFrente,  LOW);
-    analogWrite(esqTras,    velocidadeE);
+    analogWrite(esqTras,    velocidade);
 }
  
 // --- Protótipo das funções auxiliares ---
@@ -72,15 +69,7 @@ void test_channels();      //Testa os 8 canais do Turnigy9x
 //variáveis para os canais do rádio
 int canal_01 = 0, 
     canal_02 = 0, 
-    canal_03 = 0, 
-   /* canal_04 = 0, 
-    canal_05 = 0, 
-    canal_06 = 0, 
-    canal_07 = 0, 
-    canal_08 = 0; 
-    
- */
-    
+    canal_03 = 0;
     
 // --- Rotina de Interrupção ---
 ISR(TIMER2_OVF_vect)
@@ -103,12 +92,7 @@ void setup()
   pinMode(ch1, INPUT); //Entrada para o canal 1 do rádio
   pinMode(ch2, INPUT); //Entrada para o canal 2 do rádio
   pinMode(ch3, INPUT); //Entrada para o canal 3 do rádio
-/*  pinMode(ch4, INPUT); //Entrada para o canal 4 do rádio
-  pinMode(ch5, INPUT); //Entrada para o canal 5 do rádio
-  pinMode(ch6, INPUT); //Entrada para o canal 6 do rádio
-  pinMode(ch7, INPUT); //Entrada para o canal 7 do rádio
-  pinMode(ch8, INPUT); //Entrada para o canal 8 do rádio
-  */
+
   pinMode(LED, OUTPUT); //saída para o LED onboard
   
   digitalWrite(LED, LOW); //LED inicia desligado
@@ -145,30 +129,44 @@ void loop()
     test_channels(); //Testa os canais e envia informação para o Serial Monitor
     
   if(canal_01 > 1500) //direita 
-    direita();
+  { 
+    velocidade=map(canal_01,1500, 1553,0,254);
+    direita(velocidade);
+  }
   else 
     if(canal_01 < 1100) //esquerda
-      esquerda();
+    {
+      velocidade=map(canal_01,1015,1100 ,0,254);
+      esquerda(velocidade);
+    }
   if(canal_02 > 1500) //cima
-    frente();
+  {  
+    velocidade=map(canal_02,1500, 1553,0,254);
+    frente(velocidade);
+  }
   else 
     if(canal_02 < 1100) //baixo
-      tras();
+    {
+      velocidade=map(canal_02,1500, 1553,0,254);
+      tras(velocidade);
+    }
+
+  //arma
   if(canal_03 > 1550)
   {
-    analogWrite(armFrente,  velocidadeD);
+    analogWrite(armFrente,  velocidadeG);
     analogWrite(armTras,    LOW);
 
-    analogWrite(armFrente,  velocidadeE);
+    analogWrite(armFrente,  velocidadeG);
     analogWrite(armTras,    LOW);
   }
   else 
     if(canal_03 < 1100)
     {
-      analogWrite(armTras,  velocidadeD);
+      analogWrite(armTras,  velocidadeG);
       analogWrite(armFrente,    LOW);
 
-      analogWrite(armTras,  velocidadeE);
+      analogWrite(armTras,  velocidadeG);
       analogWrite(armFrente,    LOW);
     }  
 }//end loop
@@ -179,12 +177,6 @@ void read_channels() //Faz a leitura dos 6 primeiros canais do rádio
   canal_01 = pulseIn(ch1, HIGH, 25000); //Lê pulso em nível alto do canal 1 e armazena na variável canal_01
   canal_02 = pulseIn(ch2, HIGH, 25000); //Lê pulso em nível alto do canal 2 e armazena na variável canal_02
   canal_03 = pulseIn(ch3, HIGH, 25000); //Lê pulso em nível alto do canal 3 e armazena na variável canal_03
-  /*canal_04 = pulseIn(ch4, HIGH, 25000); //Lê pulso em nível alto do canal 4 e armazena na variável canal_04
-  canal_05 = pulseIn(ch5, HIGH, 25000); //Lê pulso em nível alto do canal 5 e armazena na variável canal_05
-  canal_06 = pulseIn(ch6, HIGH, 25000); //Lê pulso em nível alto do canal 6 e armazena na variável canal_06
-  canal_07 = pulseIn(ch7, HIGH, 25000); //Lê pulso em nível alto do canal 7 e armazena na variável canal_07
-  canal_08 = pulseIn(ch8, HIGH, 25000); //Lê pulso em nível alto do canal 8 e armazena na variável canal_08
-*/
 } //end read channels
 
 void test_channels() //Testa os canais via serial monitor (comentar esta função e só chamar quando necessário)
@@ -196,17 +188,7 @@ void test_channels() //Testa os canais via serial monitor (comentar esta funçã
       Serial.print(" | ");
       Serial.print(canal_03);
       Serial.print(" | ");
-/*      Serial.print(canal_04);
-      Serial.print(" | ");
-      Serial.print(canal_05);
-      Serial.print(" | ");
-      Serial.print(canal_06);
-      Serial.print(" | ");
-      Serial.print(canal_07);
-      Serial.print(" | ");
-      Serial.print(canal_08);
-      Serial.print(" | ");
-      */
+
       Serial.println("");
       Serial.println("");
       Serial.print("Canal:  ");
@@ -216,16 +198,7 @@ void test_channels() //Testa os canais via serial monitor (comentar esta funçã
       Serial.print("|");
       Serial.print("  03  ");
       Serial.print("|");
-     /* Serial.print("  04  ");
-      Serial.print("|");
-      Serial.print("  05  ");
-      Serial.print("|");
-      Serial.print("  06  ");
-      Serial.print("|");
-      Serial.print("  07  ");
-      Serial.print("|");
-      Serial.print("  08  ");
-      Serial.print("|");*/
+
       Serial.println("");
       Serial.println("");
 }
